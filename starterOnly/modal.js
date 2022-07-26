89b06b21daad6b3b63028e6e6eac6d8cb512adc3
Tristan <tristan.humelhans57@gmail.com>
2022-07-26 22:12:36 +0200

    ajout des messages d'erreurs quand formulaire pas valide

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,6 +8,40 @@ const successModal = document.getElementById("successModal");
 const checkBoxChecked = document.getElementById("checkbox1");
 const radioChecked = document.querySelectorAll("input[name=location]");
 
+console.log(formData);
+
+// Regex elements
+const birthDateRegex =
+  /(200[0-4]|19[2-9]\d)\-(1[0-2]|0[1-9])\-(3[0-1]|[0-2]\d)/g;
+const nameRegex = /^[a-zA-Z\u00e0-\u00ff]+(([- ])?[a-z\u00e0-\u00ff])+$/;
+const emailRegex =
+  /(^[a-z\d]+[\.\-\_]?[a-z\d]+)@([a-z\d]+[.\-]?[a-z\d]+)\.[a-z]+$/g;
+const quantityRegex = /^\d?\d$/g;
+
+// Array used for regex test
+const inputValue = [
+  {
+    id: "first",
+    isValid: (value) => nameRegex.test(value),
+  },
+  {
+    id: "last",
+    isValid: (value) => nameRegex.test(value),
+  },
+  {
+    id: "email",
+    isValid: (value) => emailRegex.test(value),
+  },
+  {
+    id: "birthdate",
+    isValid: (value) => birthDateRegex.test(value),
+  },
+  {
+    id: "quantity",
+    isValid: (value) => quantityRegex.test(value),
+  },
+];
+
 // responsive navigation menu
 function editNav() {
   if (burgerNav.className === "topnav") {
@@ -17,20 +51,45 @@ function editNav() {
   }
 }
 
-//check if data is empty
-function isFormDataEmpty() {
-  for (const element of formData) {
-    if (!element.children[2].value) {
-      element.setAttribute("data-error-visible", "true");
+
+/**
+ * regexTest return an Object for Each inputValue { inputValue.Id: inputValue.isValid }
+ *
+ * @return { inputValue.Id: inputValue.isValid } 
+ */
+
+function regexTest() {
+  const inputsValidity = {};
+
+  inputValue.forEach((input) => {
+    inputsValidity[input.id] = input.isValid(
+      document.getElementById(input.id).value
+    );
+  });
+
+  return inputsValidity;
+}
+
+//check if data is valid
+function isFormDataValid() {
+  const regexTestArray = regexTest();
+  let isValid;
+  for (const element in regexTestArray) {
+    if (!regexTestArray[element]) {
+      document
+        .getElementById(element)
+        .parentNode.setAttribute("data-error-visible", "true");
+        isValid = false;
+      }
     }
-  }
+    return isValid;
 }
 
 // Check if a radio is checked
 function isRadioChecked() {
   for (const element of radioChecked) {
-    
     if (!element.checked) {
+      document.getElementById("radioData").setAttribute("data-error-visible", "true");
     }
   }
 }
@@ -38,7 +97,7 @@ function isRadioChecked() {
 // Check if CGU checkbox is checked
 function isCheckboxChecked() {
   if (!checkBoxChecked.checked) {
-
+    document.getElementById("checkboxData").setAttribute("data-error-visible", "true");
   }
 }
 
